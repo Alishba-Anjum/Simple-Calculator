f9dc158b5175df6fb614f3c4bc943e7ae81568e1
Alishba-Anjum <alishbaahmedkhan11@gmail.com>
2024-05-10 00:27:49 +0500

    commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-"# Simple-Calculator" 
+# Simple Calculator
+
+This is a simple web-based calculator implemented in JavaScript. It allows users to perform basic arithmetic calculations.
+
+## Features
+
+- Addition
+- Subtraction
+- Multiplication
+- Division
+
+
---
+++ main.js
@@ -0,0 +1,24 @@
+let input = document.querySelector("input");
+
+function display(num) {
+  input.value += num;
+}
+
+function clearDisplay() {
+  input.value = "";
+}
+function del() {
+  input.value = input.value.slice(0, -1);
+}
+
+function calculate() {
+  try {
+    input.value = eval(input.value);
+  } catch (error) {
+    input.style.color = "red";
+    input.value = "ERROR";
+    setTimeout(() => {
+      input.value = "";
+    }, 1000);
+  }
+}
